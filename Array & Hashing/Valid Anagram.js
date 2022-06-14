2d7e19423eb6f49f9245908ced32697425f0ba4f
MdMainul <mainulislamfaruqi@gmail.com>
2022-06-15 01:14:26 +0600

    two sum with js

--- Array & Hashing/Valid Anagram.js
+++ Array & Hashing/Valid Anagram.js
@@ -13,7 +13,7 @@
   
   for (let i=0; i<s.length; i++) {
       countS[s[i]] = 1 + (countS[s[i]] ? +countS[s[i]] : 0)
-      countT[t[i]] = 1 + (countT[t[i]] ? countT[t[i]] : 0)
+      countT[t[i]] = 1 + (countT[t[i]] ? +countT[t[i]] : 0)
       
   }
   
